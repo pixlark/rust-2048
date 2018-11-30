0c68095102826b24877484824b20ec260e018967
Paul Tilley <t1000.pixlark@gmail.com>
2018-11-29 20:28:45 -0500

    numbers

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,14 @@ use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::video::WindowContext;
 
+use sdl2::ttf;
+
 type WindowCanvas = sdl2::render::Canvas<sdl2::video::Window>;
 
-const TILE_SIZE: u32 = 128;
-const TILE_PADDING: u32 = 16;
+const TILE_SIZE: usize = 128;
+const TILE_PADDING: usize = 16;
 
-const WINDOW_SIZE: u32 = (TILE_SIZE * 4) + (TILE_PADDING * 5);
+const WINDOW_SIZE: usize = (TILE_SIZE * 4) + (TILE_PADDING * 5);
 
 lazy_static! {
     static ref color_map: HashMap<u64, Color> = {
@@ -40,7 +42,10 @@ struct v2 {
 
 impl v2 {
     fn new(row: i32, column: i32) -> v2 {
-        v2 { row: row, column: column }
+        v2 {
+            row: row,
+            column: column,
+        }
     }
 }
 
@@ -92,32 +97,63 @@ impl Grid {
             grid: [[0; GRID_SIZE]; GRID_SIZE],
         }
     }
-    fn draw(&self, canvas: &mut WindowCanvas) {
+    fn at(&self, place: &v2) -> u64 {
+        self.grid[place.row as usize][place.column as usize]
+    }
+    fn draw(&self, canvas: &mut WindowCanvas, font: &ttf::Font) {
+        fn grid_to_real(pos: (usize, usize)) -> (usize, usize) {
+            (
+                pos.0 * (TILE_SIZE + TILE_PADDING) + TILE_PADDING,
+                pos.1 * (TILE_SIZE + TILE_PADDING) + TILE_PADDING
+            )
+        }
         canvas.set_draw_color(Color::RGB(0xCD, 0xC0, 0xB4));
         for row in 0..GRID_SIZE {
             for column in 0..GRID_SIZE {
+                // Draw squares
                 if self.grid[row][column] == 0 {
                     canvas.set_draw_color(Color::RGB(0xCD, 0xC0, 0xB4));
                 } else {
                     canvas.set_draw_color(*color_map.get(&self.grid[row][column]).unwrap());
                 }
+                let pos = grid_to_real((column, row));
                 canvas
-                    .fill_rect(Rect::new(
-                        ((column as i32) * (TILE_SIZE + TILE_PADDING) as i32)
-                            + (TILE_PADDING as i32),
-                        ((row as i32) * (TILE_SIZE + TILE_PADDING) as i32) + (TILE_PADDING as i32),
-                        TILE_SIZE,
-                        TILE_SIZE,
-                    )).unwrap();
+                    .fill_rect(Rect::new(pos.0 as i32, pos.1 as i32, TILE_SIZE as u32, TILE_SIZE as u32))
+                    .unwrap();
+                // Draw numbers
+                if self.grid[row][column] > 0 {
+                    let texture_creator = canvas.texture_creator();
+                    let texture: sdl2::render::Texture = {
+                        let text = format!("{}", self.grid[row][column]);
+                        let partial = font.render(text.as_str());
+                        let surface = partial.blended(Color::RGB(0x78, 0x6f, 0x66)).unwrap();
+                        texture_creator
+                            .create_texture_from_surface(surface)
+                            .unwrap()
+                    };
+                    let texture_size = {
+                        let query = texture.query();
+                        (query.width, query.height)
+                    };
+                    canvas.copy(
+                        &texture,
+                        None,
+                        Some(Rect::new(
+                            pos.0 as i32 + (TILE_SIZE as i32 / 2) - (texture_size.0 as i32 / 2),
+                            pos.1 as i32 + (TILE_SIZE as i32 / 2) - (texture_size.1 as i32 / 2),
+                            texture_size.0,
+                            texture_size.1,
+                        )),
+                    );
+                }
             }
         }
     }
     fn shift_block(&mut self, place: v2, dir: v2) -> bool {
-        if self.grid[place.row as usize][place.column as usize] != 0 {
-            if self.grid[(place.row + dir.row) as usize][(place.column + dir.column) as usize] == 0
-            {
-                self.grid[(place.row + dir.row) as usize][(place.column + dir.column) as usize] =
-                    self.grid[place.row as usize][place.column as usize];
+        let peek = v2::new(place.row + dir.row, place.column + dir.column);
+        if self.at(&place) != 0 {
+            if self.at(&peek) == 0 {
+                self.grid[peek.row as usize][peek.column as usize] = self.at(&place);
                 self.grid[place.row as usize][place.column as usize] = 0;
                 true
             } else {
@@ -128,7 +164,6 @@ impl Grid {
         }
     }
     fn shift_columns(&mut self, start: usize, end: usize, dir: i32) {
-        //println!("shifting columns {}", dir);
         if start as i32 + dir < 0
             || start as i32 + dir >= GRID_SIZE as i32
             || end as i32 + dir < 0
@@ -140,7 +175,7 @@ impl Grid {
             let mut changes_made = false;
             for column in BetterRange::new(start as i32, end as i32) {
                 for row in 0..GRID_SIZE {
-                    if self.shift_block(v2::new(row as i32, column), v2::new(row as i32, column + dir)) {
+                    if self.shift_block(v2::new(row as i32, column), v2::new(0, dir)) {
                         changes_made = true;
                     }
                 }
@@ -150,7 +185,28 @@ impl Grid {
             }
         }
     }
-    fn shift_rows(&mut self, start: usize, end: usize, dir: i32) {}
+    fn shift_rows(&mut self, start: usize, end: usize, dir: i32) {
+        if start as i32 + dir < 0
+            || start as i32 + dir >= GRID_SIZE as i32
+            || end as i32 + dir < 0
+            || end as i32 + dir >= GRID_SIZE as i32
+        {
+            panic!("Invalid start or end for direction");
+        }
+        loop {
+            let mut changes_made = false;
+            for column in 0..GRID_SIZE {
+                for row in BetterRange::new(start as i32, end as i32) {
+                    if self.shift_block(v2::new(row, column as i32), v2::new(dir, 0)) {
+                        changes_made = true;
+                    }
+                }
+            }
+            if !changes_made {
+                break;
+            }
+        }
+    }
     fn shift(&mut self, dir: Direction) {
         match dir {
             Direction::West => self.shift_columns(1, GRID_SIZE - 1, -1),
@@ -159,10 +215,30 @@ impl Grid {
             Direction::South => self.shift_rows(GRID_SIZE - 2, 0, 1),
         }
     }
+    fn is_full(&self) -> bool {
+        for row in 0..GRID_SIZE {
+            for column in 0..GRID_SIZE {
+                if self.at(&v2::new(row as i32, column as i32)) == 0 {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     fn insert_random_tile(&mut self) {
+        if self.is_full() {
+            return;
+        }
         let mut rng = thread_rng();
-        let row = rng.gen_range(0, GRID_SIZE);
-        let column = rng.gen_range(0, GRID_SIZE);
+        let mut row;
+        let mut column;
+        loop {
+            row = rng.gen_range(0, GRID_SIZE);
+            column = rng.gen_range(0, GRID_SIZE);
+            if self.at(&v2::new(row as i32, column as i32)) == 0 {
+                break;
+            }
+        }
         let choice: u64 = *rng.choose(&[2, 4]).unwrap();
         self.grid[row][column] = choice;
     }
@@ -170,14 +246,24 @@ impl Grid {
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
-    let video_system = sdl_context.video().unwrap();
-    let sdl_image_context = image::init(image::InitFlag::all()).unwrap();
-    let window = video_system
-        .window("SDL2 from Rust", WINDOW_SIZE, WINDOW_SIZE)
-        .position_centered()
-        .build()
-        .unwrap();
-    let mut canvas = window.into_canvas().build().unwrap();
+    let ttf_context: ttf::Sdl2TtfContext = ttf::init().unwrap();
+
+    let font = {
+        let font_bytes = include_bytes!("../resources/ClearSans-Medium.ttf");
+        ttf_context
+            .load_font_from_rwops(sdl2::rwops::RWops::from_bytes(font_bytes).unwrap(), 72)
+            .unwrap()
+    };
+
+    let mut canvas = {
+        let video_system = sdl_context.video().unwrap();
+        let window = video_system
+            .window("SDL2 from Rust", WINDOW_SIZE as u32, WINDOW_SIZE as u32)
+            .position_centered()
+            .build()
+            .unwrap();
+        window.into_canvas().build().unwrap()
+    };
 
     let mut grid = Grid::empty();
     grid.insert_random_tile();
@@ -196,6 +282,7 @@ fn main() {
                     ..
                 } => {
                     grid.shift(Direction::North);
+                    grid.insert_random_tile();
                 }
                 Event::KeyDown {
                     keycode: Some(Keycode::A),
@@ -206,6 +293,7 @@ fn main() {
                     ..
                 } => {
                     grid.shift(Direction::West);
+                    grid.insert_random_tile();
                 }
                 Event::KeyDown {
                     keycode: Some(Keycode::S),
@@ -216,6 +304,7 @@ fn main() {
                     ..
                 } => {
                     grid.shift(Direction::South);
+                    grid.insert_random_tile();
                 }
                 Event::KeyDown {
                     keycode: Some(Keycode::D),
@@ -226,15 +315,16 @@ fn main() {
                     ..
                 } => {
                     grid.shift(Direction::East);
+                    grid.insert_random_tile();
                 }
                 _ => (),
             }
         }
         canvas.set_draw_color(Color::RGB(0xBB, 0xAD, 0xA0));
         canvas
-            .fill_rect(Rect::new(0, 0, WINDOW_SIZE, WINDOW_SIZE))
+            .fill_rect(Rect::new(0, 0, WINDOW_SIZE as u32, WINDOW_SIZE as u32))
             .unwrap();
-        grid.draw(&mut canvas);
+        grid.draw(&mut canvas, &font);
         canvas.present();
     }
 }
