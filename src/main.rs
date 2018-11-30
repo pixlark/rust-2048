397209c7947358cce5f4e6d6138d91f806d3c6ae
Paul Tilley <t1000.pixlark@gmail.com>
2018-11-29 22:08:03 -0500

    windows build working

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use std::collections::HashMap;
 use rand::{thread_rng, Rng};
 
 use sdl2::event::Event;
-use sdl2::image;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
